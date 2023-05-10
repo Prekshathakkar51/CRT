cd9a25435a0cc5ae4ef9a0c91a4b7dedc1fad9cf
Preksha Thakkar <prekshathakkar5@gmail.com>
2023-05-10 09:44:34 +0530

    added new

---
+++ CTS/nearest_to_13.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int num1, num2, k1, k2;
+    printf("Enter the number1: ");
+    scanf("%d",&num1);
+    printf("Enter the number2: ");
+    scanf("%d",&num2);
+
+    k1 = abs(num1-13);
+    k2 = abs(num2-13);
+
+    if(k1<k2){
+        printf("%d", num1);
+    }
+    else{
+        printf("%d", num2);
+    }
+
+    return 0;
+}
