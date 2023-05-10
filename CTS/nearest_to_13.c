c18d1373f55f5cdf53d86ac72baaa7a229a72183
Preksha Thakkar <prekshathakkar5@gmail.com>
2023-05-10 09:52:39 +0530

    added

--- CTS/nearest_to_13.c
+++ CTS/nearest_to_13.c
@@ -11,7 +11,10 @@ int main(){
     k1 = abs(num1-13);
     k2 = abs(num2-13);
 
-    if(k1<k2){
+    if((num1>13) && (num2>13)){
+        printf("0");
+    }
+    else if(k1<k2){
         printf("%d", num1);
     }
     else{
